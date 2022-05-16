fc4cba51b3656cae72a8ade84b82a6f3ae4c8078
jnissenbaum18 <jnissenbaum@verizon.net>
2022-05-16 15:29:32 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('James N')
+})
+
+app.get('/search', (req, res) => {
+    res.send("search page")
+})
+
+app.get('*', (req, res)=> {
+    res.send("404")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
